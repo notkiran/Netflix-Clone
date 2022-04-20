82d9225107e3496971c7119acf473bdf0e41feea
Kiran G <mail@kirang.me>
2022-04-20 13:25:58 +0530

    register

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import "./app.scss";
 import Home from "./Pages/Home/Home";
+import Register from "./Pages/Register/Register";
 
 function App() {
   return (
     <div>
-      <Home />
+      <Register />
     </div>
   );
 }
