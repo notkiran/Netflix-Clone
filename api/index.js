1c186e5563280f5ebc706d970b059fb95f09ee7e
Kiran G <mail@kirang.me>
2022-04-20 15:06:44 +0530

    register

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 dotenv.config();
+const authRoute = require("./routes/auth");
+
+app.use(express.json());
 
 const mongoose = require("mongoose");
 
@@ -12,6 +15,8 @@ mongoose
   })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.log(err));
+
+app.use("/api/auth", authRoute);
 
 app.listen(8800, () => {
   console.log("Server is running on port 8800");
---
+++ api/routes/auth.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJs = require("crypto-js");
+
+//REGISTER
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJs.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString(),
+  });
+
+  try {
+    const user = await newUser.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//LOGIN
+router.post("/login", async (req, res) => {
+    try {
+      
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
