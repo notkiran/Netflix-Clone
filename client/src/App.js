a4473dcb7112931fdce437b710f014d0492e9efd
Kiran G <mail@kirang.me>
2022-04-20 13:54:45 +0530

    final frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,10 @@
 import "./app.scss";
 import Home from "./Pages/Home/Home";
-import Register from "./Pages/Register/Register";
 
 function App() {
   return (
     <div>
-      <Register />
+      <Home />
     </div>
   );
 }
--- client/src/Pages/Watch/Watch.jsx
+++ client/src/Pages/Watch/Watch.jsx
@@ -10,10 +10,11 @@ const Watch = () => {
       </div>
       <video
         className="video"
-        src="https://vod-progressive.akamaized.net/exp=1650311490~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F543%2F22%2F552718079%2F2614711873.mp4~hmac=b74fbc874f31b5ed236ca6137cbfeefd97f93e5a89e822df21b3f1363df8b0dd/vimeo-prod-skyfire-std-us/01/543/22/552718079/2614711873.mp4?download=1&filename=pexels-taryn-elliott-7964548.mp4"
         autoPlay
+        muted
         progress
         controls
+        src="https://kirang.me/assets/vid/netflx-vid.mp4"
       />
     </div>
   );
