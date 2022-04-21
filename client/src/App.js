d7ae39dfdaf6ddd9cc094ff557974dc00d8a798a
Kiran G <mail@kirang.me>
2022-04-21 14:49:44 +0530

    react route

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,52 @@
 import "./app.scss";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Home from "./Pages/Home/Home";
+import Watch from "./Pages/Watch/Watch";
+import Register from "./Pages/Register/Register";
+import Login from "./Pages/Login/Login";
 
 function App() {
+  const user = true;
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={user ? <Home /> : <Navigate to="/register" />}
+        />
+      </Routes>
+      <Routes>
+        <Route
+          path="/register"
+          element={!user ? <Register /> : <Navigate to="/" />}
+        />
+      </Routes>
+      <Routes>
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/" />}
+        />
+      </Routes>
+      {user && (
+        <>
+          <Routes>
+            <Route path="/movies" element={<Home type="movies" />} />
+          </Routes>
+          <Routes>
+            <Route path="/series" element={<Home type="series" />} />
+          </Routes>
+          <Routes>
+            <Route path="/watch" element={<Watch />} />
+          </Routes>
+        </>
+      )}
+    </Router>
   );
 }
 
--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -3,11 +3,11 @@ import List from "../../components/List/List";
 import Navbar from "../../components/Navbar/Navbar";
 import "./home.scss";
 
-const Home = () => {
+const Home = ({ type }) => {
   return (
     <div className="home">
       <Navbar />
-      <Featured />
+      <Featured type={type} />
       <List />
       <List />
       <List />
